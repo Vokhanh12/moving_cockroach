d40fe6b81fa625302877a1c419b69d25000747ea
Khánh Nguyễn Võ <khanhyou2018@gmail.com>
2023-06-29 11:44:46 +0700

    add commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import khongImage from './loai-gian-my.jpg';
 
-function App() {
+const App = () => {
+  const [positionY, setPositionY] = useState(0);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key === 'ArrowUp') {
+        setPositionY((prevPosition) => prevPosition - 50);
+      } else if (event.key === 'ArrowDown') {
+        setPositionY((prevPosition) => prevPosition + 50);
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img
+          src={khongImage}
+          className="rotate-image"
+          alt="khong"
+          style={{ transform: `translateY(${positionY}px)` }}
+        />
       </header>
     </div>
   );
-}
+};
 
 export default App;
